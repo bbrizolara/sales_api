4e9356b8cd2b6e2dc9c55c6767457fd3804c2309
bbrizolara <bbrizolara@codigodelsur.com>
2021-10-19 18:51:06 -0300

    First steps

---
+++ src/shared/errors/app_error.ts
@@ -0,0 +1,11 @@
+class AppError {
+  public readonly message: string;
+  public readonly statusCode: number;
+
+  constructor(message: string, statudCode = 400) {
+    this.message = message;
+    this.statusCode = statudCode;
+  }
+}
+
+export default AppError;
