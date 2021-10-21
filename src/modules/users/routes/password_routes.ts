b83e8b2f310f81b949f646252a2489697b575f3b
bbrizolara <bbrizolara@codigodelsur.com>
2021-10-21 11:41:23 -0300

    Adding forgot and reset password

---
+++ src/modules/users/routes/password_routes.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import { celebrate, Joi, Segments } from "celebrate";
+import forgotPasswordController from "../controllers/forgot_password_controller";
+import resetPasswordController from "../controllers/reset_password_controller";
+
+const passwordRouter = Router();
+
+passwordRouter.post(
+  "/forgot",
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+    },
+  }),
+  forgotPasswordController.create
+);
+
+passwordRouter.post(
+  "/reset",
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      passwordConfirmation: Joi.string().required().valid(Joi.ref("password")),
+    },
+  }),
+  resetPasswordController.create
+);
+
+export default passwordRouter;
